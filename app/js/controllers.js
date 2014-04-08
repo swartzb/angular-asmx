32da68b6506bb046473e7e3804ee58a1e5a60af3
Bill Swartz <swartzbill2000@yahoo.com>
2014-04-08 14:09:50 -0600

    added css class, routing changes, employees list work

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,7 +10,16 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('EmployeesController', ['$scope', '$http', function ($scope, $http) {
-    $scope.hasSupervisor = function (employee) { return employee.Supervisor; };
+    $scope.toggleSelect = function (employee) {
+      var ndx, len;
+      if (!employee.isSelected) {
+        for (ndx = 0, len = $scope.employees.length; ndx < len; ++ndx) {
+          $scope.employees[ndx].isSelected = false;
+        };
+      };
+      employee.isSelected = !employee.isSelected;
+      return;
+    };
 
     $http({
       url: '../NwndSvc.asmx/GetAllEmployees',
@@ -18,7 +27,12 @@ angular.module('myApp.controllers', [])
       data: "{}",
       headers: { 'Content-Type': 'application/json' }
     }).success(function (data, status, headers, config) {
+      var ndx, len;
       $scope.employees = data.d;
+      for (ndx = 0, len = $scope.employees.length; ndx < len; ++ndx) {
+        $scope.employees[ndx].isSelected = false;
+      };
+      return;
     }).error(function (data, status, headers, config) {
       $scope.data = data;
     });
