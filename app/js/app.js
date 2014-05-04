e98d6faaca0e4b501a8a22c5cbf5f54945a63dee
Bill Swartz <swartzbill2000@yahoo.com>
2014-05-04 15:28:08 -0600

    routing changes

--- app/js/app.js
+++ app/js/app.js
@@ -12,6 +12,6 @@ angular.module('myApp', [
 config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/view1', { templateUrl: 'partials/partial1.html', controller: 'MyCtrl1' });
   $routeProvider.when('/view2', { templateUrl: 'partials/partial2.html', controller: 'MyCtrl2' });
-  $routeProvider.when('/employees', { templateUrl: 'partials/employees.html', controller: 'EmployeesController' });
+  $routeProvider.when('/employees/load/:loadVal', { templateUrl: 'partials/employees.html', controller: 'EmployeesController' });
   $routeProvider.otherwise({ redirectTo: '/view1' });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,46 +2,49 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', ['myApp.services'])
+angular.module('myApp.controllers', ['ngRoute', 'myApp.services'])
   .controller('MyCtrl1', [function () {
 
   }])
   .controller('MyCtrl2', [function () {
 
   }])
-  .controller('EmployeesController', ['$scope', '$window', 'northwindService', function ($scope, $window, northwindService) {
-    $scope.northwind = northwindService;
+  .controller('EmployeesController', ['$scope', '$routeParams', '$window', 'northwindService',
+    function ($scope, $routeParams, $window, northwindService) {
+      $scope.northwind = northwindService;
 
-    $scope.addNewEmployee = function () {
-      $window.alert('Add New Employee ');
-      return;
-    };
+      $scope.addNewEmployee = function () {
+        $window.alert('Add New Employee ');
+        return;
+      };
 
-    $scope.editSelectedEmployee = function () {
-      if ($scope.northwind.selectedEmployee) {
-        $window.alert('Edit ' + $scope.northwind.selectedEmployee.LastName + ', ' + $scope.northwind.selectedEmployee.FirstName);
+      $scope.editSelectedEmployee = function () {
+        if ($scope.northwind.selectedEmployee) {
+          $window.alert('Edit ' + $scope.northwind.selectedEmployee.LastName + ', ' + $scope.northwind.selectedEmployee.FirstName);
+        }
+        return;
+      };
+
+      $scope.deleteSelectedEmployee = function () {
+        if ($scope.northwind.selectedEmployee) {
+          $window.alert('Delete ' + $scope.northwind.selectedEmployee.LastName + ', ' + $scope.northwind.selectedEmployee.FirstName);
+        }
+        return;
+      };
+
+      $scope.viewSelectedEmployeeDetails = function () {
+        if ($scope.northwind.selectedEmployee) {
+          $window.alert('View Details for ' + $scope.northwind.selectedEmployee.LastName + ', ' + $scope.northwind.selectedEmployee.FirstName);
+        }
+        return;
+      };
+
+      $scope.toggleSelect = function (employee) {
+        $scope.northwind.selectedEmployee = (employee == $scope.northwind.selectedEmployee) ? null : employee;
+        return;
+      };
+
+      if ($routeParams.loadVal) {
+        northwindService.getAllEmployees();
       }
-      return;
-    };
-
-    $scope.deleteSelectedEmployee = function () {
-      if ($scope.northwind.selectedEmployee) {
-        $window.alert('Delete ' + $scope.northwind.selectedEmployee.LastName + ', ' + $scope.northwind.selectedEmployee.FirstName);
-      }
-      return;
-    };
-
-    $scope.viewSelectedEmployeeDetails = function () {
-      if ($scope.northwind.selectedEmployee) {
-        $window.alert('View Details for ' + $scope.northwind.selectedEmployee.LastName + ', ' + $scope.northwind.selectedEmployee.FirstName);
-      }
-      return;
-    };
-
-    $scope.toggleSelect = function (employee) {
-      $scope.northwind.selectedEmployee = (employee == $scope.northwind.selectedEmployee) ? null : employee;
-      return;
-    };
-
-    northwindService.getAllEmployees();
-  }]);
+    }]);
