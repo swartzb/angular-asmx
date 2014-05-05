bc69805d5f96d305cc6098bd949d9c3fa15bb82b
Bill Swartz <swartzbill2000@yahoo.com>
2014-05-05 13:17:33 -0600

    file and directory changes

--- app/js/app.js
+++ app/js/app.js
@@ -3,15 +3,9 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
+  'myApp.routes',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/view1', { templateUrl: 'partials/partial1.html', controller: 'MyCtrl1' });
-  $routeProvider.when('/view2', { templateUrl: 'partials/partial2.html', controller: 'MyCtrl2' });
-  $routeProvider.when('/employees/load/:loadVal', { templateUrl: 'partials/employees.html', controller: 'EmployeesController' });
-  $routeProvider.otherwise({ redirectTo: '/view1' });
-}]);
+]);
--- app/js/routes.js
+++ app/js/routes.js
@@ -2,12 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+angular.module('myApp.routes', [
+  'ngRoute'
 ]).
 config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/view1', { templateUrl: 'partials/partial1.html', controller: 'MyCtrl1' });
