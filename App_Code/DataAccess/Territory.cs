353723dd8596a78e5d7044c369f54504cd5cab3e
Bill Swartz <swartzbill2000@yahoo.com>
2014-07-07 21:07:36 -0600

    this time I really added it

---
+++ App_Code/DataAccess/Territory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess
+{
+  /// <summary>
+  /// Summary description for Territory
+  /// </summary>
+  public class Territory
+  {
+    public string TerritoryID { set; get; }
+
+    public string TerritoryDescription { set; get; }
+
+    public Territory()
+    {
+
+    }
+
+    public Territory(Territory t)
+    {
+      this.TerritoryDescription = t.TerritoryDescription;
+      this.TerritoryID = t.TerritoryID;
+    }
+  }
+
+}
