ee232852bd5349f5ebe99de7c269f6782c0124f7
Bill Swartz <swartzbill2000@yahoo.com>
2014-10-03 06:08:53 -0600

    cleanup in DataAccess/EmployeeSummary.cs

--- App_Code/DataAccess/EmployeeSummary.cs
+++ App_Code/DataAccess/EmployeeSummary.cs
@@ -19,10 +19,6 @@ namespace DataAccess
     }
 
     public int EmployeeID { get; set; }
-    public string LastName { get; set; }
-    public string FirstName { get; set; }
-    public string Title { get; set; }
-    public string TitleOfCourtesy { get; set; }
     public string Name { get; set; }
     public DateTime? HireDate { get; set; }
     public string Notes { get; set; }
